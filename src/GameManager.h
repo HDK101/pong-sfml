96978218527f879eb39973b7eeec06f1ff2ed6bf
Lucas Hideki <lucashideki87@gmail.com>
2019-12-17 10:26:52 -0300

    Implemented member for header GameManager

--- src/GameManager.h
+++ src/GameManager.h
@@ -1,7 +1,18 @@
 #ifndef GAMEMANAGER_H
 #define GAMEMANAGER_H
+
+#include "Pad.h"
+
 class GameManager
 {
-
-}
+private:
+	sf::Vector2f &ballPosition;
+    int playerOnePoints,playerTwoPoints;
+public:
+	GameManager();
+	int getPlayerOnePoints();
+	int getPlayerTwoPoints();
+	void Restart();
+	void IncreasePoints(CurrentPlayer player);
+};
 #endif
